4e1eb1ffbd8bd6945c75569e8793f6c87979b593
MaksimShubelko <shubelkom@gmail.com>
2023-06-11 01:59:31 +0300

    Init

---
+++ bin/custom/customextension/gensrc/org/customextension/constants/GeneratedCustomextensionConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jun 11, 2023, 12:58:46 AM                   ---
+ * ----------------------------------------------------------------
+ */
+package org.customextension.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedCustomextensionConstants
+{
+	public static final String EXTENSIONNAME = "customextension";
+	
+	protected GeneratedCustomextensionConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
